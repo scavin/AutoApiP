335b687a8ae81af9c07f4f6eef9727535fb2277f
scavin <scavina@gmail.com>
2021-07-16 17:09:22 +0800

    Update runapi.py

--- runapi.py
+++ runapi.py
@@ -2,7 +2,7 @@
 import os
 import requests as req
 import json,sys,time,random
-
+global access_token_list
 app_num=os.getenv('APP_NUM')
 if app_num == '':
     app_num = '1'
@@ -46,7 +46,7 @@ api_list = [r'https://graph.microsoft.com/v1.0/me/',
             ]
 
 #微软refresh_token获取
-def getmstoken(ms_token,appnum):
+def getmstoken(ms_token):
     headers={'Content-Type':'application/x-www-form-urlencoded'
             }
     data={'grant_type': 'refresh_token',
@@ -57,10 +57,6 @@ def getmstoken(ms_token,appnum):
         }
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
-    if 'refresh_token' in jsontxt:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取成功')
-    else:
-        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取失败'+'\n'+'请检查secret里 CLIENT_ID , CLIENT_SECRET , MS_TOKEN 格式与内容是否正确，然后重新设置')
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     return access_token
@@ -85,16 +81,17 @@ def runapi(apilist,a):
 
 #一次性获取access_token，降低获取率
 for a in range(1, int(app_num)+1):
+    
     if a == 1: 
         client_id=os.getenv('CLIENT_ID')
         client_secret=os.getenv('CLIENT_SECRET')
         ms_token=os.getenv('MS_TOKEN')
-        access_token_list[a-1]=getmstoken(ms_token,a)
+        access_token_list[a-1]=getmstoken(ms_token)
     else:
         client_id=os.getenv('CLIENT_ID_'+str(a))
         client_secret=os.getenv('CLIENT_SECRET_'+str(a))
         ms_token=os.getenv('MS_TOKEN_'+str(a))
-        access_token_list[a-1]=getmstoken(ms_token,a)
+        access_token_list[a-1]=getmstoken(ms_token)
 
 #随机api序列
 fixed_api=[0,1,5,6,20,21]
@@ -106,10 +103,7 @@ random.shuffle(fixed_api)
 final_list=fixed_api
 
 #实际运行
-if int(app_num) > 1:
-    print('多账户/应用模式下，日志报告里可能会出现一堆***，属于正常情况')
-print("如果api数量少于规定值，则是api赋权没有弄好，或者是onedrive还没有初始化成功。前者请重新赋权并获取微软密钥替换，后者请稍等几天")
-print('共 '+str(app_num)+r' 账号/应用，'+r'每个账号/应用 '+str(config_list['每次轮数'])+' 轮') 
+print('共'+str(config_list['每次轮数'])+'轮')    
 for c in range(1,config_list['每次轮数']+1):
     if config_list['是否启动随机时间'] == 'Y':
         time.sleep(random.randint(config_list['延时范围起始'],config_list['结束']))		
@@ -119,24 +113,24 @@ for c in range(1,config_list['每次轮数']+1):
         if a==1:
             client_id=os.getenv('CLIENT_ID')
             client_secret=os.getenv('CLIENT_SECRET')
-            print('\n'+'应用/账号 '+str(a)+' 的第'+str(c)+'轮 '+time.asctime(time.localtime(time.time()))+'\n')
+            print('\n'+'应用/账号 '+str(a)+' 的第'+str(c)+'轮'+time.asctime(time.localtime(time.time()))+'\n')
             if config_list['是否开启随机api顺序'] == 'Y':
-                print("已开启随机顺序,共十二个api,自己数")
+                print("已开启随机顺序,共12个api,自己数")
                 apilist=final_list
                 runapi(apilist,a)
             else:
-                print("原版顺序,共十个api,自己数")
+                print("原版顺序,共10个api,自己数")
                 apilist=[5,9,8,1,20,24,23,6,21,22]
                 runapi(apilista,a)
         else:
             client_id=os.getenv('CLIENT_ID_'+str(a))
             client_secret=os.getenv('CLIENT_SECRET_'+str(a))
-            print('\n'+'应用/账号 '+str(a)+' 的第'+str(c)+'轮 '+time.asctime(time.localtime(time.time()))+'\n')
+            print('\n'+'应用/账号 '+str(a)+' 的第'+str(c)+'轮'+time.asctime(time.localtime(time.time()))+'\n')
             if config_list['是否开启随机api顺序'] == 'Y':
-                print("已开启随机顺序,共十二个api,自己数")
+                print("已开启随机顺序,共12个api,自己数")
                 apilist=final_list
                 runapi(apilist,a)
             else:
-                print("原版顺序,共十个api,自己数")
+                print("原版顺序,共10个api,自己数")
                 apilist=[5,9,8,1,20,24,23,6,21,22]
                 runapi(apilist,a)
